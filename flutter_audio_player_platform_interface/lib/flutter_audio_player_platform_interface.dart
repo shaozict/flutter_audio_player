67f750efdeac23a44c832e51aa0a33be836f6c7c
TENCENT\dansun <dansun@tencent.com>
2021-11-08 11:58:23 +0800

    add code

--- flutter_audio_player_platform_interface/lib/flutter_audio_player_platform_interface.dart
+++ flutter_audio_player_platform_interface/lib/flutter_audio_player_platform_interface.dart
@@ -3,15 +3,45 @@
 // To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
 // directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
-import 'dart:async';
+import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/services.dart';
+import 'method_channel_audio_player.dart';
 
-class FlutterAudioPlayerPlatformInterface {
-  static const MethodChannel _channel = MethodChannel('flutter_audio_player_platform_interface');
+abstract class AudioPlayerPlatform {
+  /// The default instance of [AudioPlayerPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelAudioPlayer].
+  static AudioPlayerPlatform get instance => _instance;
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [MethodChannelAudioPlayer] when they register themselves.
+  static set instance(AudioPlayerPlatform value) {
+    if (!value.isMock) {
+      try {
+        value._verifyProvidesDefaultImplementations();
+      } on NoSuchMethodError catch (_) {
+        throw AssertionError(
+            'Platform interfaces must not be implemented with `implements`');
+      }
+    }
+    _instance = value;
   }
+
+  static AudioPlayerPlatform _instance =
+  MethodChannelAudioPlayer();
+
+  /// Only mock implementations should set this to true.
+  ///
+  /// Mockito mocks are implementing this class with `implements` which is forbidden for anything
+  /// other than mocks (see class docs). This property provides a backdoor for mockito mocks to
+  /// skip the verification that the class isn't implemented with `implements`.
+  @visibleForTesting
+  bool get isMock => false;
+
+  Future<void> init()async{}
+  //播放
+  Future<void> play()async{}
+  Future<void> pause()async{}
+  Future<void> stop()async{}
+  void _verifyProvidesDefaultImplementations() {}
 }
---
+++ flutter_audio_player_platform_interface/lib/method_channel_audio_player.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_audio_player_platform_interface/flutter_audio_player_platform_interface.dart';
+
+class MethodChannelAudioPlayer extends AudioPlayerPlatform{
+
+}
