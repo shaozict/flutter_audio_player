0553534ea746baee649991f2629ada684a5303cb
ajiejyang <ajiejyang@tencent.com>
2022-02-28 17:26:53 +0800

    stash 支持windows

--- flutter_audio_player/example/windows/flutter/generated_plugin_registrant.cc
+++ flutter_audio_player/example/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,8 @@
 #include "generated_plugin_registrant.h"
 
 #include <dart_vlc/dart_vlc_plugin.h>
-#include <flutter_audio_player_windows/audio_player_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DartVlcPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DartVlcPlugin"));
-  AudioPlayerWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("AudioPlayerWindows"));
 }
